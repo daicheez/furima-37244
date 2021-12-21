eb66c20a3524027ea69b657f7c66bc894037a51c
daicheez <dai.rotty.2@gmail.com>
2021-12-21 18:27:28 +0900

    ログイン・ログアウト機能リンクの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,23 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth_day, presence: true
   validates :first_name, presence: true, format: {
-                                          with: /\A[ぁ-んァ-ン一-龥]/,
-                                          message: "全角で入力して下さい"
-                                        }
+    with: /\A[ぁ-んァ-ン一-龥]/,
+    message: '全角で入力して下さい'
+  }
   validates :last_name, presence: true, format: {
-                                          with: /\A[ぁ-んァ-ン一-龥]/,
-                                          message: "全角で入力して下さい"
-                                        }
+    with: /\A[ぁ-んァ-ン一-龥]/,
+    message: '全角で入力して下さい'
+  }
   validates :kana_first_name, presence: true, format: {
-                                                with: /\A[ァ-ヶー－]+\z/,
-                                                message: "全角カタカナで入力して下さい"
-                                              }
+    with: /\A[ァ-ヶー－]+\z/,
+    message: '全角カタカナで入力して下さい'
+  }
   validates :kana_last_name, presence: true, format: {
-                                              with: /\A[ァ-ヶー－]+\z/,
-                                              message: "全角カタカナで入力して下さい"
-                                            }
+    with: /\A[ァ-ヶー－]+\z/,
+    message: '全角カタカナで入力して下さい'
+  }
+  validates :encrypted_password, presence: true, format: {
+    with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/,
+    message: 'アルファベットと数字を含めて下さい'
+  }
 end
