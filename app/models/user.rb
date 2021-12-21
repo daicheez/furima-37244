b0ae5a68901a2403a271c6b45cf1c0687297579c
daicheez <dai.rotty.2@gmail.com>
2021-12-21 22:02:25 +0900

    Rspec,FactoryBot,Faker導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :birth_day, presence: true
-  validates :first_name, presence: true, format: {
+  validates :nickname, :birth_day, presence: true
+  validates :first_name, :last_name, presence: true, format: {
     with: /\A[ぁ-んァ-ン一-龥]/,
     message: '全角で入力して下さい'
   }
-  validates :last_name, presence: true, format: {
-    with: /\A[ぁ-んァ-ン一-龥]/,
-    message: '全角で入力して下さい'
-  }
-  validates :kana_first_name, presence: true, format: {
+
+  validates :kana_first_name, :kana_last_name, presence: true, format: {
     with: /\A[ァ-ヶー－]+\z/,
     message: '全角カタカナで入力して下さい'
   }
-  validates :kana_last_name, presence: true, format: {
-    with: /\A[ァ-ヶー－]+\z/,
-    message: '全角カタカナで入力して下さい'
-  }
+
   validates :encrypted_password, presence: true, format: {
     with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/,
     message: 'アルファベットと数字を含めて下さい'
