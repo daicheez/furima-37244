a5753c226e94603882512345a5900402f81dacf2
daicheez <dai.rotty.2@gmail.com>
2021-12-21 15:58:28 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :birth_day, presence: true
-  validates :first_name, :last_name, presence: true,
-                                      format: {
-                                        with: /\A[ぁ-んァ-ン一-龥]/,
-                                        message: "全角で入力して下さい"
-                                      }
-  validates :kana_first_name, :kana_last_name, presence: true,
-                                                format: {
-                                                  with: /\A[ァ-ヶー－]+\z/,
-                                                  message: "全角カタカナで入力して下さい"
-                                                }
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+  validates :first_name, presence: true, format: {
+                                          with: /\A[ぁ-んァ-ン一-龥]/,
+                                          message: "全角で入力して下さい"
+                                        }
+  validates :last_name, presence: true, format: {
+                                          with: /\A[ぁ-んァ-ン一-龥]/,
+                                          message: "全角で入力して下さい"
+                                        }
+  validates :kana_first_name, presence: true, format: {
+                                                with: /\A[ァ-ヶー－]+\z/,
+                                                message: "全角カタカナで入力して下さい"
+                                              }
+  validates :kana_last_name, presence: true, format: {
+                                              with: /\A[ァ-ヶー－]+\z/,
+                                              message: "全角カタカナで入力して下さい"
+                                            }
 end
