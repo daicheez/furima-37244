940fb14a12c2be64b9b135e16bb93887eb6611c8
daicheez <dai.rotty.2@gmail.com>
2021-12-22 11:55:41 +0900

    Gimei導入とFakerの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,17 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
+    transient do
+      person {Gimei.name}
+    end
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
-    password {password}
+    password              {password}
     password_confirmation {password}
-    first_name {}
-    last_name
-    first_name_kana
-    last_name_kana
-    birth_day
+    first_name            {person.first.kanji}
+    last_name             {person.last.kanji}
+    first_name_kana       {person.first.katakana}
+    last_name_kana        {person.last.katakana}
+    birth_day             {Faker::Date.between(from: '1930-1-1', to: '2016-12-31')}
   end
 end
