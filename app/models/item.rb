e02a5845aa76d0f85c9a60b1e6494d64104d6400
daicheez <dai.rotty.2@gmail.com>
2021-12-23 16:29:34 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,11 @@ class Item < ApplicationRecord
   has_one :purchase_record
   has_one_attached :image
 
-  validates :item_name, :item_detail, :price, presence: true
+  validates :item_name, :item_detail, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :price, presence: true, format: {
+    with: /\A[0-9]+\z/
+  }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
