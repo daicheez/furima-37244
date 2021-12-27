0e8105e165e10f4f5d9dca41e522275e54543b84
daicheez <dai.rotty.2@gmail.com>
2021-12-27 12:25:56 +0900

    編集機能へのルーティング、コントローラー、ビューファイル記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :go_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,7 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
@@ -28,5 +40,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :image, :item_name, :item_detail, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def go_to_index
+    redirect_to root_path unless current_user == @item.user
   end
 end
