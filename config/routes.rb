62865c25fd7bdf2549819962ef0385a1086dcb50
daicheez <dai.rotty.2@gmail.com>
2021-12-30 19:33:44 +0900

    不要なルートを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/create'
   devise_for :users
-  get 'items/index'
   root to: "items#index"
   resources :items do
     resources :orders, only:[:index, :create]
